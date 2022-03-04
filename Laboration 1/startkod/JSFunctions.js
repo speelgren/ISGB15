0e34e1b34b12cbf5929051d53985e0f7cd27bdc3
speelgren <97473666+speelgren@users.noreply.github.com>
2022-03-04 13:33:31 +0100

    Labb 1

---
+++ Laboration 1/startkod/JSFunctions.js
@@ -0,0 +1,69 @@
+"use strict";
+
+/**
+ * Globalt objekt som innehåller de attribut som ni skall använda.
+ * Initieras genom anrop till funktionern initGlobalObject().
+ */
+let oGameData = {};
+
+/**
+ * Initerar det globala objektet med de attribut som ni skall använda er av.
+ * Funktionen tar inte emot några värden.
+ * Funktionen returnerar inte något värde.
+ */
+oGameData.initGlobalObject = function() {
+
+    //Datastruktur för vilka platser som är lediga respektive har brickor
+    oGameData.gameField = Array('', '', '', '', '', '', '', '', '');
+    
+    /* Testdata för att testa rättningslösning */
+    //oGameData.gameField = Array('X', 'X', 'X', '', '', '', '', '', '');
+    //oGameData.gameField = Array('X', '', '', 'X', '', '', 'X', '', '');
+    //oGameData.gameField = Array('X', '', '', '', 'X', '', '', '', 'X');
+    //oGameData.gameField = Array('', '', 'X', '', 'X', '', 'X', '', '');
+    //oGameData.gameField = Array('X', 'O', 'X', '0', 'X', 'O', 'O', 'X', 'O');
+
+    //Indikerar tecknet som skall användas för spelare ett.
+    oGameData.playerOne = "X";
+
+    //Indikerar tecknet som skall användas för spelare två.
+    oGameData.playerTwo = "O";
+
+    //Kan anta värdet X eller O och indikerar vilken spelare som för tillfället skall lägga sin "bricka".
+    oGameData.currentPlayer = "";
+
+    //Nickname för spelare ett som tilldelas från ett formulärelement,
+    oGameData.nickNamePlayerOne = "";
+
+    //Nickname för spelare två som tilldelas från ett formulärelement.
+    oGameData.nickNamePlayerTwo = "";
+
+    //Färg för spelare ett som tilldelas från ett formulärelement.
+    oGameData.colorPlayerOne = "";
+
+    //Färg för spelare två som tilldelas från ett formulärelement.
+    oGameData.colorPlayerTwo = "";
+
+    //"Flagga" som indikerar om användaren klickat för checkboken.
+    oGameData.timerEnabled = false;
+
+    //Timerid om användaren har klickat för checkboxen. 
+    oGameData.timerId = null;
+
+}
+
+/**
+ * Kontrollerar för tre i rad.
+ * Returnerar 0 om det inte är någon vinnare, 
+ * returnerar 1 om spelaren med ett kryss (X) är vinnare,
+ * returnerar 2 om spelaren med en cirkel (O) är vinnare eller
+ * returnerar 3 om det är oavgjort.
+ * Funktionen tar inte emot några värden.
+ */
+oGameData.checkForGameOver = function() {
+
+   
+
+}
+
+
