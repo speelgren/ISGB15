38b5f8655f371fac37929db84fa40572d11f5099
speelgren <97473666+speelgren@users.noreply.github.com>
2022-05-20 00:23:18 +0200

    Uppgift 4

---
+++ Tenta/Tenta 202204214/Uppgift 4/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+window.addEventListener('load', () => {
+
+  console.log('DOM loaded....');
+  addClickListenersToButtons();
+});
+
+const addClickListenersToButtons = () => {
+
+  let button = document.querySelectorAll('button');
+
+  button.forEach(knapp => {
+
+    knapp.addEventListener('click', (event) => {
+
+      event.preventDefault();
+
+      let value = event.target.getAttribute('data-add');
+      let valueNode = document.createTextNode(value);
+      let list = document.querySelector('.list');
+      let p = document.createElement('p');
+
+      p.appendChild(valueNode);
+      list.appendChild(p);
+
+      p.addEventListener('click', (event) => {
+
+        event.target.remove();
+      });
+
+      p.addEventListener('mouseover', (event) => {
+
+        event.target.style.fontWeight = 'bold';
+        event.target.classList.add('font-weight-bold');
+      });
+
+      p.addEventListener('mouseout', (event) => {
+
+        event.target.style.fontWeight = 'normal';
+        event.target.classList.remove('font-weight-bold');
+      });
+    });
+  });
+}
