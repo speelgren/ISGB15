a30d3580c58d79fc058938a0080b1bc5d2237e30
speelgren <97473666+speelgren@users.noreply.github.com>
2022-05-19 23:39:47 +0200

    uppgift 3

--- Tenta/Tenta 202204214/Uppgift 3/index.js
+++ Tenta/Tenta 202204214/Uppgift 3/index.js
@@ -2,14 +2,28 @@
 
 window.addEventListener('load', () => {
 
-  console.log('DOM loaded');
+  document.querySelector('form').addEventListener('submit', validate);
 });
 
+const validate = (event) => {
 
-document.querySelector('form').addEventListener('submit', validate);
+  try {
 
-function validate(event) {
+    let email = document.querySelector('#email').value;
+    let fisk = document.querySelector('input[value="fish"]');
+    let klocka = document.querySelector('#klockslag').value
 
-  console.log('validate');
-  event.preventDefault();
+    if(email.length < 6 ) throw new Error ('För kort email');
+    if(fisk.checked != true) throw new Error ('Välj fisk');
+    if(klocka != '8') throw new Error ('Välj kl 8');
+
+  } catch(error) {
+
+    let felmeddelande = document.createTextNode(error);
+    let err = document.querySelector('#error');
+    err.style.textDecoration = 'underline red';
+    err.appendChild(felmeddelande);
+
+    event.preventDefault();
+  }
 };
